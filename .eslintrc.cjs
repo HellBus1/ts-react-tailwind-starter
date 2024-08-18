b70c0c038bb14f6125010d49eb64d656d64f4b0e
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-08-18 20:40:09 +0700

    feat : add code formatter & style lint to make sure all code have similar format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true, es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -8,11 +10,22 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true,
+    },
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+    sourceType: 'module'
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'comma-dangle': ['error', 'never'],
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
   },
 }
