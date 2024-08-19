d5eab0794d1655ad106c96c3ffd923486e0ad797
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-08-19 10:45:05 +0700

    feat : add husky & lint-staged git to make sure to check format & style lint before commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import { routes } from "./routes";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import { routes } from './routes'
 
-const AppRouter = createBrowserRouter([
-  ...routes
-]);
-
+const AppRouter = createBrowserRouter([...routes])
 
 function App() {
-  return (
-    <RouterProvider router={AppRouter} />
-  );
+  return <RouterProvider router={AppRouter} />
 }
 
-export default App;
+export default App
